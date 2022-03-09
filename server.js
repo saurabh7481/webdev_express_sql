34e6e6b08152f0cf83d57816c75c9e9470b540d8
saurabh <saurabh7481@gmail.com>
2022-03-09 19:45:40 +0530

    added file read/write

--- server.js
+++ server.js
@@ -1,34 +1,60 @@
 const http = require("http");
+const fs = require("fs");
+
+// const server = http.createServer((req, res) => {
+//     const url = req.url;
+//     const method = req.method;
+//     if (url === '/') {
+//       res.write('<html>');
+//       res.write('<head><title>Enter Message</title><head>');
+//       res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+//       res.write('</html>');
+//       return res.end();
+//     }
+//     if (url === '/message' && method === 'POST') {
+//       const body = [];
+//       req.on('data', (chunk) => {
+//         body.push(chunk);
+//       });
+//       req.on('end', () => {
+//         const parsedBody = Buffer.concat(body).toString();
+//         const message = parsedBody.split('=')[1];
+//         fs.writeFileSync('message.txt', message);
+//       });
+//       res.statusCode = 302;
+//       res.setHeader('Location', '/');
+//       return res.end();
+//     }
+// })
 
 const server = http.createServer((req, res) => {
-    if(req.url === "/"){
-        res.setHeader("Content-Type", "text/html");
-        res.write("<html>")
-        res.write("<head><title>NodeJS</title></head>")
-        res.write("<body><h2>Welcome to NodeJS</h2></body>")
-        res.write("</html>")
-        return res.end();
-    } else if(req.url === "/home"){
-        res.setHeader("Content-Type", "text/html");
-        res.write("<html>")
-        res.write("<head><title>NodeJS</title></head>")
-        res.write("<body><h2>Welcome to Home Page</h2></body>")
-        res.write("</html>")
-        return res.end();
-    } else if(req.url === "/aboutus"){
-        res.setHeader("Content-Type", "text/html");
-        res.write("<html>")
-        res.write("<head><title>NodeJS</title></head>")
-        res.write("<body><h2>Welcome to About us page</h2></body>")
-        res.write("</html>")
-        res.end();
-    } else if(req.url === "/node"){
-        res.setHeader("Content-Type", "text/html");
-        res.write("<html>")
-        res.write("<head><title>NodeJS</title></head>")
-        res.write("<body><h2>Welcome to my node project</h2></body>")
-        res.write("</html>")
-        res.end();
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+      res.write('<html>');
+      res.write('<head><title>Enter Message</title><head>');
+      res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+      res.write('</html>');
+      return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+      const body = [];
+      req.on('data', (chunk) => {
+        body.push(chunk);
+      });
+      req.on('end', () => {
+        const parsedBody = Buffer.concat(body).toString();
+        const message = parsedBody.split('=')[1];
+        fs.writeFile('message.txt', message, err => {
+            fs.readFile("message.txt", "utf8", (err, data) => {
+                res.write('<html>');
+                res.write('<head><title>Enter Message</title><head>');
+                res.write(`<body><h2>${data}</h2></body>`);
+                res.write('</html>');
+                return res.end();
+            })
+        });
+      });
     }
 })
 
