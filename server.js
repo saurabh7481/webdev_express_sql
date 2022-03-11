cc422113c63c381af612f9630de92ee84eb85097
saurabh <saurabh7481@gmail.com>
2022-03-11 18:56:23 +0530

    added express:

--- server.js
+++ server.js
@@ -1,39 +1,15 @@
-const http = require("http");
-const handler = require("./routes");
+const express = require("express");
+const app = express();
 
+app.use((req, res, next) => {
+    console.log("Middleware 1");
+    next();
+})
 
-const server = http.createServer(handler);
+app.use((req, res, next) => {
+    console.log("Middleware 2");
+    res.send("<h1>Hello</h1>")
+})
 
+app.listen(3000);
 
-// const server = http.createServer((req, res) => {
-//     const url = req.url;
-//     const method = req.method;
-//     if (url === '/') {
-//       res.write('<html>');
-//       res.write('<head><title>Enter Message</title><head>');
-//       res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-//       res.write('</html>');
-//       return res.end();
-//     }
-//     if (url === '/message' && method === 'POST') {
-//       const body = [];
-//       req.on('data', (chunk) => {
-//         body.push(chunk);
-//       });
-//       req.on('end', () => {
-//         const parsedBody = Buffer.concat(body).toString();
-//         const message = parsedBody.split('=')[1];
-//         fs.writeFile('message.txt', message, err => {
-//             fs.readFile("message.txt", "utf8", (err, data) => {
-//                 res.write('<html>');
-//                 res.write('<head><title>Enter Message</title><head>');
-//                 res.write(`<body><h2>${data}</h2></body>`);
-//                 res.write('</html>');
-//                 return res.end();
-//             })
-//         });
-//       });
-//     }
-// })
-
-server.listen(3000);
