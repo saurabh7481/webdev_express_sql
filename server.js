c32b57317fc382eb0c3d556fd015195a9e48401e
saurabh <saurabh7481@gmail.com>
2022-03-13 11:16:20 +0530

    added middlewares/routes

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("Middleware 1");
-    next();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use("/add_product", (req, res, next) => {
+    res.send("<form action='/product' method='POST'><input type='text' name='title'/><input type='number' name='size'/><button type='submit'>Add</button></form>");
 })
 
-app.use((req, res, next) => {
-    console.log("Middleware 2");
+app.post("/product", (req, res, next) => {
+    console.log(req.body);
+    res.redirect("/");
+})
+
+app.use("/", (req, res) => {
     res.send("<h1>Hello</h1>")
 })
 
